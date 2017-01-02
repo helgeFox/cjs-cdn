7d2820292243776a889eff2bce42acfded40dbaa
Helge Hofstad <helge@foxpublish.com>
2017-01-02 17:16:19 +0100

    stupid copy-paste doesn't convert indents... Need .editorconfig file, if that might help

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,24 @@ var rename = require('gulp-rename');
 var pump = require('pump');
 
 var files = [
-	'./node_modules/moment/min/moment.min.js', 
-	'./node_modules/moment/locale/nb.js',
-	'./src/nytid-script.js'
+  './node_modules/moment/min/moment.min.js', 
+  './node_modules/moment/locale/nb.js',
+  './src/nytid-script.js'
 ];
 var scriptName = 'nytid-cjs-scripts.js';
 var destination = './dist/';
  
 gulp.task('scripts', function() {
   gulp.src(files)
-  	.pipe(concat(scriptName))
+    .pipe(concat(scriptName))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-  	.pipe(gulp.dest(destination))
+    .pipe(gulp.dest(destination))
 });
 
+// Procedure for release:
+//  1. In terminal navigate to ./dist
+//  2. Type 'now'
+//  3. (Optional) Type 'now alias ' + the newly created adress + ' cjs-cdn.now.sh'
 
 gulp.task('default', ['scripts']);
