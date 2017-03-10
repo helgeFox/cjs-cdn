6f71be85129bf5f057cc22192601d2fbad9f7cb0
Helge Hofstad <helge@foxpublish.com>
2017-03-10 16:44:57 +0100

    fix gulp: add 'clean' task and upgrade gulp to make certain tasks synchronous

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,79 @@
+'use strict';
+
 var gulp = require('gulp');
+var del = require('del');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var exec = require('child_process').exec;
 
-var files = [
-	'./node_modules/moment/min/moment.min.js', 
-	'./node_modules/moment/locale/nb.js',
-	'./src/nytid-script.js'
-];
-var scriptName = 'nytid-cjs-scripts.js';
-var assets = [
-	'./src/icons/*'
-];
 var destination = './dist/';
+var paths = {
+	scripts: {
+		src: [
+			'./node_modules/moment/min/moment.min.js', 
+			'./node_modules/moment/locale/nb.js',
+			'./src/nytid-script.js'
+		],
+		name: 'nytid-cjs-scripts.js',
+		dest: destination
+	},
+	assets: {
+		src: [
+			'./src/icons/*'
+		],
+		dest: destination + '/icons'
+	}
+};
 
-gulp.task('scripts', function() {
-	gulp.src(files)
-	.pipe(concat(scriptName))
+function clean () {
+	return del([destination + '**/*']);
+}
+
+function scripts () {
+	return gulp.src(paths.scripts.src)
+	.pipe(concat(paths.scripts.name))
 	.pipe(rename({suffix: '.min'}))
 	.pipe(uglify())
-	.pipe(gulp.dest(destination));
-});
+	.pipe(gulp.dest(paths.scripts.dest));
+}
 
-gulp.task('assets', function () {
-	gulp.src(assets)
-	.pipe(gulp.dest(destination + '/icons'));
-});
+function assets () {
+	return gulp.src(paths.assets.src)
+	.pipe(gulp.dest(paths.assets.dest));
+}
 
-gulp.task('pkg', function () {
+function pkg () {
 	// Wait... Deploying *with* the package file requires more setup. Read https://zeit.co/blog/now-static
 	// gulp.src('./package.json')
 	// .pipe(gulp.dest(destination));
-});
+}
 
-gulp.task('deploy', function () {
-	exec('now -n cjs-cdn --static', {cwd: './dist'}, function (err, stdout, stderr) {
-		if (err) {
-			console.error('Deploy failed... ', err);
-			return;
-		}
-		console.log('Deploy to now.sh successful\nURL: ' + stdout);
-		exec('now alias set ' + stdout + ' cjs-cdn.now.sh', {cwd: './dist'}, function (err, stdout, stderr) {
+function deploy() {
+	return new Promise(function(resolve, reject) {
+		exec('now -n cjs-cdn --static', {cwd: './dist'}, function (err, stdout, stderr) {
 			if (err) {
-				console.error('Now-alias failed!', err);
+				console.error('Deploy failed... ', err);
+				reject(err);
 				return;
 			}
-			console.log('Now-alias successful');
+			console.log('Deploy to now.sh successful\nURL: ' + stdout);
+			exec('now alias set ' + stdout.replace('https://', '') + ' cjs-cdn.now.sh', {cwd: './dist'}, function (err, stdout, stderr) {
+				if (err) {
+					console.error('Now-alias failed!', err);
+					reject(err);
+					return;
+				}
+				console.log('Now-alias successful');
+				resolve();
+			});
 		});
 	});
-});
+}
+gulp.task('deploy', deploy);
 
-gulp.task('default', ['scripts', 'assets']);
-gulp.task('release', ['scripts', 'assets', 'pkg', 'deploy']);
+var build = gulp.series(clean, gulp.parallel(scripts, assets));
+var release = gulp.series(build, deploy);
+
+gulp.task('default', release);
+gulp.task('release', release);
